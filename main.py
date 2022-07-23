243b28d4ae9802f7cc320dc9b0570305d3acb4c7
Rouzh Jaafar <103349444+Roswtta@users.noreply.github.com>
2022-07-23 22:31:26 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#first_name = 'Rouzh'
+#last_name = 'Jaafar'
+#full_name = first_name + ' ' + last_name
+#print(full_name)
+
+#age = 21
+#age += 1
+#print("Your age is: "+str(age))
+
+#height = 250.5
+#print("Your height is: "+str(height) + "cm")
+#print(type(height))
+
+human = True
+print("Are you a human: " +str(human))
+print(type(human))
